cf6437d9a4ad3968b5584796f1ed4b62189a033b
ikuchi0824 <64464207+ikuchi0824@users.noreply.github.com>
2020-07-31 03:12:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  // Intialize
+  $(".menu").hide();
+  $(".menu-child").hide();
+  $('.menu>li').has('.menu-child').append('<span class="dropButton"></span>');
+  $(".dropButton").addClass('isClosed');
+
+  // Toggle navButton
+  $(".navButton").click( function(){
+    $(".menu").slideToggle();
+  });
+
+  // Toggle dropButton
+  $(".dropButton").click( function() {
+
+    if( $(this).hasClass('isClosed')) {
+
+      $(this).removeClass('isClosed');
+      $(this).addClass('isOpen');
+      $(this).prev(".menu-child").slideDown(500);
+
+      $(".dropButton").not(this).removeClass('isOpen');
+      $(".dropButton").not(this).addClass('isClosed');
+      $(".dropButton").not(this).prev(".menu-child").slideUp(500);
+
+    } else {
+
+      $(this).removeClass('isOpen');
+      $(this).addClass('isClosed');
+      $(this).prev(".menu-child").slideUp(500);
+
+    }
+  });
+
+});
